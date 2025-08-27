dbf1bead0c0e68a5a5665c4239dc4196db0aca14
Wendi Bacon <44605769+nomadscientist@users.noreply.github.com>
2025-08-27 09:20:57 +0100

    wrap text for prettier

--- src/organisms/OrganisationsSubsidiaries/OrganisationsSubsidiaries.tsx
+++ src/organisms/OrganisationsSubsidiaries/OrganisationsSubsidiaries.tsx
@@ -53,7 +53,8 @@ export default function OrganisationsSubsidiaries({
       <PageSection heading={t("heading")}>
         <div>
           <Typography sx={{ mb: 2 }}>
-            If your Organisation is based internationally, please add any UK-based subsidiaries below.
+            If your Organisation is based internationally, please add any 
+            UK-based subsidiaries below.
           </Typography>
           <OrganisationsSubsidiariesTable
             data={organisation?.subsidiaries || []}
