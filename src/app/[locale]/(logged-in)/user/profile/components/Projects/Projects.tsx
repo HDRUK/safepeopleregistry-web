3e16d7d1e98bb27af12f340956ba7287f0b15ded
Peter Hammans <peter.hammans@hdruk.ac.uk>
2025-09-03 11:54:09 +0100

    feat(REGISTRY-1823): project description

--- src/app/[locale]/(logged-in)/user/profile/components/Projects/Projects.tsx
+++ src/app/[locale]/(logged-in)/user/profile/components/Projects/Projects.tsx
@@ -2,14 +2,14 @@ import { PageBody, PageBodyContainer } from "@/modules";
 import ProjectsList from "@/organisms/Projects";
 import { useTranslations } from "next-intl";
 
-const NAMESPACE_TRANSLATION = "Projects";
+const NAMESPACE_TRANSLATION = "Profile.Projects";
 
 export default function Projects() {
   const t = useTranslations(NAMESPACE_TRANSLATION);
 
   return (
-    <PageBodyContainer heading={t("projects")}>
-      <PageBody>
+    <PageBodyContainer heading={t("heading")}>
+      <PageBody description={t("description")}>
         <ProjectsList variant="user" />
       </PageBody>
     </PageBodyContainer>
