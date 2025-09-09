6ea662eb5a88a400d7745e927b816ff58178acab
Peter Hammans <peter.hammans@hdruk.ac.uk>
2025-09-09 15:23:28 +0100

    fix: remove user check

--- src/app/[locale]/(logged-in)/organisation/profile/user-administration/[subTabId]/[id]/[subSubTabId]/components/Layout/Layout.tsx
+++ src/app/[locale]/(logged-in)/organisation/profile/user-administration/[subTabId]/[id]/[subSubTabId]/components/Layout/Layout.tsx
@@ -1,6 +1,4 @@
 import { ReactNode } from "react";
-import { getUser } from "@/services/users";
-import { notFound } from "next/navigation";
 import { UserSubTabs } from "../../../../../../consts/tabs";
 import SubTabSections from "../SubTabSections";
 
@@ -12,11 +10,7 @@ interface LayoutProps {
   };
 }
 
-async function Layout({ children, params: { id, subSubTabId } }: LayoutProps) {
-  const { data } = await getUser(id);
-
-  if (!data) notFound();
-
+function Layout({ children, params: { id, subSubTabId } }: LayoutProps) {
   return (
     <>
       <SubTabSections userId={id} subTabId={subSubTabId} />
