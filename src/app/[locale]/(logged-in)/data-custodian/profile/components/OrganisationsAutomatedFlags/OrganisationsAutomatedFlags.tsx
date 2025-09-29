856fd030ed88ef3c60259d3521cfcbc2cdb6210e
Peter Hammans <peter.hammans@hdruk.ac.uk>
2025-09-29 11:24:19 +0100

    fix: some typo

--- src/app/[locale]/(logged-in)/data-custodian/profile/components/OrganisationsAutomatedFlags/OrganisationsAutomatedFlags.tsx
+++ src/app/[locale]/(logged-in)/data-custodian/profile/components/OrganisationsAutomatedFlags/OrganisationsAutomatedFlags.tsx
@@ -1,13 +1,17 @@
 import { useStore } from "@/data/store";
-import { PageSection } from "@/modules";
+import { PageBody } from "@/modules";
 import RulesCheck from "@/modules/RulesCheck";
+import { useTranslations } from "next-intl";
+
+const NAMESPACE_TRANSLATION = "CustodianProfile";
 
 export default function OrganisationsAutomatedFlags() {
+  const t = useTranslations(NAMESPACE_TRANSLATION);
   const organisation = useStore(state => state.getCurrentOrganisation());
 
   return (
-    <PageSection>
+    <PageBody heading={t("organisationsAutomatedFlags")}>
       <RulesCheck rules={organisation.rules || []} />
-    </PageSection>
+    </PageBody>
   );
 }
