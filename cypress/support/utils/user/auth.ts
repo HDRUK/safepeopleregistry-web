72c84707f1ec2f5d5a725148765fba21f91f24a1
Peter Hammans <peter.hammans@hdruk.ac.uk>
2025-10-28 16:27:38 +0000

    chore: chaneg dependencies install

--- cypress/support/utils/user/auth.ts
+++ cypress/support/utils/user/auth.ts
@@ -1,5 +1,5 @@
 const loginUser = () => {
-  cy.login(Cypress.env("userEmail"), Cypress.env("userPassword"));
+  cy.login("dan.ackroyd@ghostbusters.com", "Walk26Task!");
 };
 
 export { loginUser };
