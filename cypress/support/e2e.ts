9a3793b32897f978ce2332cc6eaa1622338ea05a
Peter Hammans <peter.hammans@hdruk.ac.uk>
2025-10-29 11:21:51 +0000

    fix: test cy run with chrome

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,9 +1,9 @@
-Cypress.on("uncaught:exception", err => {
-  return !(
-    /minified react error/i.test(err.message) ||
-    /hydrat/i.test(err.message) ||
-    /unknown/i.test(err.message)
-  );
-});
+// Cypress.on("uncaught:exception", err => {
+//   return !(
+//     /minified react error/i.test(err.message) ||
+//     /hydrat/i.test(err.message) ||
+//     /unknown/i.test(err.message)
+//   );
+// });
 
 import "./commands";
