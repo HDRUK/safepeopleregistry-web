5165b29be5c5b1eafa722589534b2f421cdf1ff9
peter-hammans <164904644+peter-hammans@users.noreply.github.com>
2025-11-24 11:32:19 +0000

    fix(REGISTRY-2273): not redirecting from admin (#562)

---
+++ src/utils/router.ts
@@ -0,0 +1,44 @@
+import { ROUTES } from "@/consts/router";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import getMe from "../services/auth/getMe";
+import { RouteConfig, Routes } from "../types/router";
+import { getAccessToken } from "./auth";
+import { getProfileRedirectPath, redirectToPath } from "./redirects";
+
+function needsLoggedInPermissions(routes: Routes, pathname: string | null) {
+  if (!pathname) return false;
+
+  return Object.keys(routes).some((key: string) => {
+    const route: RouteConfig = routes[key as keyof Routes];
+
+    return pathname.includes(route.path, 0) && route.permissions;
+  });
+}
+
+function getPathServerSide(): string | null {
+  const head = headers();
+  return head.get("x-current-path");
+}
+
+async function redirectProfile() {
+  const pathname = getPathServerSide();
+
+  const accessToken = await getAccessToken();
+
+  if (!accessToken && needsLoggedInPermissions(ROUTES, pathname)) {
+    redirect(ROUTES.homepage.path);
+  }
+
+  if (accessToken && pathname) {
+    const response = await getMe({
+      suppressThrow: true,
+    });
+
+    if (response.status === 200) {
+      redirectToPath(getProfileRedirectPath(response.data), pathname);
+    }
+  }
+}
+
+export { getPathServerSide, needsLoggedInPermissions, redirectProfile };
