e635a02923dacb94a12159acaa9365000d0597bc
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-11-25 10:04:03 +0000

    lnt

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,7 +3,7 @@ import { getRequestConfig } from "next-intl/server";
 import { locales } from "./config";
 
 export default getRequestConfig(async ({ requestLocale }) => {
-  let locale = await requestLocale;
+  const locale = await requestLocale;
 
   const defaultLocale = process.env.NEXT_INTL_DEFAULT_LOCALE || "en";
   const resolvedLocale = locale ?? defaultLocale;
