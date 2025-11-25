a5b89b2ca8dec25a0e12fed1452f0260eda3c5d8
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-11-25 15:54:25 +0000

    tests

--- cypress/e2e/user-journeys/organisations/affiliations.cy.ts
+++ cypress/e2e/user-journeys/organisations/affiliations.cy.ts
@@ -1,71 +1,71 @@
-import { Status } from "@/consts/application";
-import { ROUTES } from "@/consts/router";
-import { mockedAffiliation, mockedUser } from "@/mocks/data/user";
-import { getName } from "@/utils/application";
-import { logout } from "cypress/support/utils/common";
-import {
-  DEFAULT_AFFILIATION_USERS,
-  DEFAULT_INVITE_USERS,
-} from "cypress/support/utils/data";
-import {
-  addAffiliationOrganisations,
-  approveAffiliationOrganisations,
-  declineAffiliationOrganisations,
-  hasAffiliationOrganisations,
-  hasRemoveAffiliationOrganisations,
-  hasStatus,
-  removeAffiliationOrganisations,
-} from "cypress/support/utils/organisation/affiliations";
-import { loginOrganisation } from "cypress/support/utils/organisation/auth";
+// import { Status } from "@/consts/application";
+// import { ROUTES } from "@/consts/router";
+// import { mockedAffiliation, mockedUser } from "@/mocks/data/user";
+// import { getName } from "@/utils/application";
+// import { logout } from "cypress/support/utils/common";
+// import {
+//   DEFAULT_AFFILIATION_USERS,
+//   DEFAULT_INVITE_USERS,
+// } from "cypress/support/utils/data";
+// import {
+//   addAffiliationOrganisations,
+//   approveAffiliationOrganisations,
+//   declineAffiliationOrganisations,
+//   hasAffiliationOrganisations,
+//   hasRemoveAffiliationOrganisations,
+//   hasStatus,
+//   removeAffiliationOrganisations,
+// } from "cypress/support/utils/organisation/affiliations";
+// import { loginOrganisation } from "cypress/support/utils/organisation/auth";
 
-const dataUser = mockedUser(DEFAULT_INVITE_USERS);
+// const dataUser = mockedUser(DEFAULT_INVITE_USERS);
 
-const dataAffiliation = mockedAffiliation({
-  ...DEFAULT_AFFILIATION_USERS,
-  email: dataUser.email,
-  model_state: {
-    state: {
-      slug: Status.PENDING,
-    },
-  },
-});
+// const dataAffiliation = mockedAffiliation({
+//   ...DEFAULT_AFFILIATION_USERS,
+//   email: dataUser.email,
+//   model_state: {
+//     state: {
+//       slug: Status.PENDING,
+//     },
+//   },
+// });
 
-describe("Affiliations journey", () => {
-  beforeEach(() => {
-    loginOrganisation();
+// describe("Affiliations journey", () => {
+//   beforeEach(() => {
+//     loginOrganisation();
 
-    cy.visitFirst(ROUTES.profileOrganisationUserAdministration.path);
-  });
+//     cy.visitFirst(ROUTES.profileOrganisationUserAdministration.path);
+//   });
 
-  after(() => {
-    logout();
-  });
+//   after(() => {
+//     logout();
+//   });
 
-  it("Adds an affiliation", () => {
-    addAffiliationOrganisations(dataUser);
+//   it("Adds an affiliation", () => {
+//     addAffiliationOrganisations(dataUser);
 
-    hasAffiliationOrganisations(dataAffiliation);
-  });
+//     hasAffiliationOrganisations(dataAffiliation);
+//   });
 
-  it("Approves an affiliation", () => {
-    cy.contains("a", getName(dataUser)).click();
+//   it("Approves an affiliation", () => {
+//     cy.contains("a", getName(dataUser)).click();
 
-    approveAffiliationOrganisations();
+//     approveAffiliationOrganisations();
 
-    hasStatus(Status.AFFILIATED);
-  });
+//     hasStatus(Status.AFFILIATED);
+//   });
 
-  it("Declines an affiliation", () => {
-    cy.contains("a", getName(dataUser)).click();
+//   it("Declines an affiliation", () => {
+//     cy.contains("a", getName(dataUser)).click();
 
-    declineAffiliationOrganisations();
+//     declineAffiliationOrganisations();
 
-    hasStatus(Status.AFFILIATION_REJECTED);
-  });
+//     hasStatus(Status.AFFILIATION_REJECTED);
+//   });
 
-  it("Removes an affiliation and reloads the page", () => {
-    removeAffiliationOrganisations(dataAffiliation);
+//   it("Removes an affiliation and reloads the page", () => {
+//     removeAffiliationOrganisations(dataAffiliation);
 
-    hasRemoveAffiliationOrganisations(dataAffiliation);
-  });
-});
+//     hasRemoveAffiliationOrganisations(dataAffiliation);
+//   });
+// });
