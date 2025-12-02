33c9d2786c15b9aa3c9a64c24b27fbf8bb271110
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-12-02 18:20:49 +0000

    tester

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,14 +1,5 @@
 Cypress.on("uncaught:exception", err => {
   return !/unknown/i.test(err.message);
 });
-Cypress.on("window:before:load", (win) => {
-  const style = win.document.createElement("style");
-  style.innerHTML = `
-    input[type="date"] {
-      -webkit-appearance: none !important;
-      -moz-appearance: textfield !important;
-    }
-  `;
-  win.document.head.appendChild(style);
-});
+
 import "./commands";
