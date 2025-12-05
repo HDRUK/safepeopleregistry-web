3211d7ec084a2da1994bbd84987829c28142b854
Peter Hammans <peter.hammans@hdruk.ac.uk>
2025-12-05 10:20:38 +0000

    fix: double click

--- cypress/e2e/user-journeys/custodians/configuration.cy.ts
+++ cypress/e2e/user-journeys/custodians/configuration.cy.ts
@@ -1,40 +1,36 @@
 import { ROUTES } from "@/consts/router";
 import { logout } from "cypress/support/utils/common";
 import { loginCustodian } from "cypress/support/utils/custodian/auth";
-import { 
-  hasCheckedOnOrganisationConfiguration, 
-  hasCheckedOnUsersConfiguration, 
-  hasUnCheckedOnOrganisationConfiguration, 
-  hasUnCheckedOnUsersConfiguration } from "cypress/support/utils/custodian/configuration";
+import {
+  hasCheckedOnOrganisationConfiguration,
+  hasCheckedOnUsersConfiguration,
+  hasUnCheckedOnOrganisationConfiguration,
+  hasUnCheckedOnUsersConfiguration,
+} from "cypress/support/utils/custodian/configuration";
 
 describe("Projects custodians journey", () => {
   beforeEach(() => {
     loginCustodian();
-
     cy.visitFirst(ROUTES.profileCustodianConfiguration.path);
-  });
-
-  before(() => {
-    loginCustodian();
   });
 
   after(() => {
     logout();
   });
 
-    it("Has Checked On Users Configuration", () => {
-        hasCheckedOnUsersConfiguration();
-      });
+  it("Has Checked On Users Configuration", () => {
+    hasCheckedOnUsersConfiguration();
+  });
 
-    it("Has Checked On Users Configuration", () => {
-      hasUnCheckedOnUsersConfiguration();
-      });
+  it("Has Checked On Users Configuration", () => {
+    hasUnCheckedOnUsersConfiguration();
+  });
 
-    it("Has Checked On Users Configuration", () => {
-      hasCheckedOnOrganisationConfiguration();
-      });
+  it("Has Checked On Users Configuration", () => {
+    hasCheckedOnOrganisationConfiguration();
+  });
 
-    it("Has Checked On Users Configuration", () => {
-      hasUnCheckedOnOrganisationConfiguration();
-      });
+  it("Has Checked On Users Configuration", () => {
+    hasUnCheckedOnOrganisationConfiguration();
+  });
 });
