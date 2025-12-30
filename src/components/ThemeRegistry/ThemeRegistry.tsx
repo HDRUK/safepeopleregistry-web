80374641adc8f51a0266aaa124a7fef0ca54d422
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-12-30 15:36:10 +0000

    remove duplicate cache

--- src/components/ThemeRegistry/ThemeRegistry.tsx
+++ src/components/ThemeRegistry/ThemeRegistry.tsx
@@ -4,7 +4,6 @@ import * as React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "@/theme";
-import EmotionCache from "./EmotionCache";
 
 export default function ThemeRegistry({
   children,
@@ -12,11 +11,9 @@ export default function ThemeRegistry({
   children: React.ReactNode;
 }) {
   return (
-    <EmotionCache options={{ key: "mui" }}>
-      <ThemeProvider theme={theme}>
+   <ThemeProvider theme={theme}>
         <CssBaseline />
         {children}
       </ThemeProvider>
-    </EmotionCache>
   );
 }
