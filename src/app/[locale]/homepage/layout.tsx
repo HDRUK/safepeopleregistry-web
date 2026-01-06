e858ed3834ec1f259880b978d4f26de5225e2a19
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2026-01-06 08:56:27 +0000

    fix(REGISTRY-2400): Blank homepage (#610)

--- src/app/[locale]/homepage/layout.tsx
+++
@@ -1,8 +0,0 @@
-import redirectApplication from "@/utils/router";
-import { PropsWithChildren } from "react";
-
-export default async function Layout({ children }: PropsWithChildren) {
-  await redirectApplication();
-
-  return children;
-}
