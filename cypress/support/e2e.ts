de04438265965e011c8c0c04802f325f3deb184b
Peter Hammans <peter.hammans@hdruk.ac.uk>
2026-01-12 09:56:48 +0000

    fix(REGISTRY-2431): revert old code

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -2,13 +2,7 @@ import "cypress-maildev";
 import "./commands";
 
 Cypress.on("uncaught:exception", err => {
-  // Ignore React/Next.js hydration mismatch errors
-  if (
-    err.message.includes("hydration") ||
-    err.message.includes("Minified React error #418")
-  ) {
-    return false;
-  }
+  return !/unknown/i.test(err.message);
 });
 
 import "./commands";
