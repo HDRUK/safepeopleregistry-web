02a48d0499cfd22fd5ee10259ce2ba37e1edc4ce
Peter Hammans <peter.hammans@hdruk.ac.uk>
2026-01-26 11:51:38 +0000

    fix: update client id

--- cypress/support/utils/mail.ts
+++ cypress/support/utils/mail.ts
@@ -1,14 +1,19 @@
+import { o } from "framer-motion/dist/types.d-B_QPEvFK";
+
 function actionMessage(
   label: string | RegExp,
   options: {
     to: string;
   }
 ) {
+  cy.log(`****** DOING WAIT`);
   cy.wait(6000);
 
+  cy.log(`****** WAITED`);
   cy.maildevGetMessageBySentTo(options.to).then(email => {
+    cy.log(`****** STARTED: ${options.to}`, email?.html);
     const emailHtml = Cypress.$(email?.html);
-
+    cy.log(`****** EMAIL: ${emailHtml.get(0).outerHTML}`);
     const link = emailHtml.find("a").filter((_, el) => {
       if (typeof label === "string") {
         return el.textContent === label;
@@ -16,6 +21,8 @@ function actionMessage(
 
       return label.test(el.textContent);
     });
+
+    cy.log(`****** EMAIL: ${emailHtml}`);
 
     const href = link.attr("href");
 
