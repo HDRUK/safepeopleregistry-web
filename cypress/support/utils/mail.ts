16d12383d099d1de0294d689c380ae837126def0
Peter Hammans <peter.hammans@hdruk.ac.uk>
2026-01-26 16:10:09 +0000

    chore: mopup logs

--- cypress/support/utils/mail.ts
+++ cypress/support/utils/mail.ts
@@ -1,19 +1,14 @@
-import { o } from "framer-motion/dist/types.d-B_QPEvFK";
-
 function actionMessage(
   label: string | RegExp,
   options: {
     to: string;
   }
 ) {
-  cy.log(`****** DOING WAIT`);
   cy.wait(6000);
 
-  cy.log(`****** WAITED`);
   cy.maildevGetMessageBySentTo(options.to).then(email => {
-    cy.log(`****** STARTED: ${options.to}`, email?.html);
     const emailHtml = Cypress.$(email?.html);
-    cy.log(`****** EMAIL: ${emailHtml.get(0).outerHTML}`);
+
     const link = emailHtml.find("a").filter((_, el) => {
       if (typeof label === "string") {
         return el.textContent === label;
@@ -22,11 +17,7 @@ function actionMessage(
       return label.test(el.textContent);
     });
 
-    cy.log(`****** EMAIL: ${emailHtml}`);
-
     const href = link.attr("href");
-
-    cy.log(`****** FOUND HREF: ${href}`);
 
     if (!href) {
       throw new Error("Email doesn't exist");
