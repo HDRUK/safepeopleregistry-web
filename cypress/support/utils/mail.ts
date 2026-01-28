bb6b1d1998f7ee271d7d1a7f7274971ffe085292
Peter Hammans <peter.hammans@hdruk.ac.uk>
2026-01-28 11:31:03 +0000

    chore: fixing maildev

--- cypress/support/utils/mail.ts
+++ cypress/support/utils/mail.ts
@@ -6,21 +6,15 @@ function actionMessage(
 ) {
   cy.wait(6000);
 
-  cy.maildevGetMessageBySentTo(options.to)
-    .debug()
-    .then(email => {
-      cy.task("log", label);
-      cy.task("log", options.to);
-      cy.task("log", JSON.stringify(email));
+  cy.maildevGetMessageBySentTo(options.to).then(email => {
+    cy.maildevVisitMessageById(email.id);
 
-      cy.maildevVisitMessageById(email.id);
-
-      cy.contains("a", label)
-        .invoke("attr", "href")
-        .then(href => {
-          cy.visit(href);
-        });
-    });
+    cy.contains("a", label)
+      .invoke("attr", "href")
+      .then(href => {
+        cy.visit(href);
+      });
+  });
 }
 
 export { actionMessage };
