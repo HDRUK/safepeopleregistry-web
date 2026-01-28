aa1ee3346fe73f30e023a0b2e017e804afc8e9c0
peter-hammans <164904644+peter-hammans@users.noreply.github.com>
2026-01-28 12:15:25 +0000

    feat(REGISTRY-2495): white screen, double register, next redirect (#637)

--- cypress/support/utils/mail.ts
+++ cypress/support/utils/mail.ts
@@ -7,23 +7,13 @@ function actionMessage(
   cy.wait(6000);
 
   cy.maildevGetMessageBySentTo(options.to).then(email => {
-    const emailHtml = Cypress.$(email?.html);
+    cy.maildevVisitMessageById(email.id);
 
-    const link = emailHtml.find("a").filter((_, el) => {
-      if (typeof label === "string") {
-        return el.textContent === label;
-      }
-
-      return label.test(el.textContent);
-    });
-
-    const href = link.attr("href");
-
-    if (!href) {
-      throw new Error("Email doesn't exist");
-    } else {
-      cy.visit(href);
-    }
+    cy.contains("a", label)
+      .invoke("attr", "href")
+      .then(href => {
+        cy.visit(href);
+      });
   });
 }
 
