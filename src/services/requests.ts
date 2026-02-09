28fa011850faecc014eb5843dde7b42501d204bb
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2026-02-09 17:19:20 +0000

    feat - test things

--- src/services/requests.ts
+++ src/services/requests.ts
@@ -48,6 +48,10 @@ async function request<T>(
       host = `${process.env.NEXT_PUBLIC_API_V1_URL}`;
     }
 
+    console.log(`${host}${url}`, '<<< url');
+    console.log(`${headers}`, '<<< headers');
+
+
     const response = await fetch(`${host}${url}`, {
       ...options,
       method,
