994c42dd76860987e9ea4e0e433ce6272f00afd4
Phil Reeks <phil.reeks@hdruk.ac.uk>
2026-03-05 15:36:36 +0000

    ODS ID

--- src/modules/OrganisationsDigitalIdentifiersDetails/OrganisationsDigitalIdentifiersDetails.tsx
+++ src/modules/OrganisationsDigitalIdentifiersDetails/OrganisationsDigitalIdentifiersDetails.tsx
@@ -41,6 +41,16 @@ export default function OrganisationsDigitalIdentifiersDetails({
             </Link>
           ),
         },
+        {
+          column_id: "ods_id",
+          content: (
+            <Link
+              target="_blank"
+              href={`https://www.odsdatasearchandexport.nhs.uk/?search=generalorg&query=${organisationData.ods_id}`}>
+              {organisationData.ods_id}
+            </Link>
+          ),
+        },
         "",
         {
           column_id: "charities",
