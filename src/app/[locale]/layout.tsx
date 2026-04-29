74b1ac43129da434d7531e3ed443669e35879366
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2026-04-29 15:13:55 +0100

    you and your numbers!

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -14,7 +14,7 @@ import {
   isTestFeatureUserAdmin,
 } from "@/flags";
 import BannerMessage from "@/modules/BannerMessage";
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v16-appRouter";
 import GlobalStyles from "@mui/material/GlobalStyles";
 import { Box } from "@mui/system";
 import { GoogleTagManager } from "@next/third-parties/google";
