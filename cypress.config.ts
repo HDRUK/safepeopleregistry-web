71d38a0449af4b318d5a5a661d62894acb800e67
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2026-04-29 15:14:45 +0100

    video

--- cypress.config.ts
+++ cypress.config.ts
@@ -86,8 +86,8 @@ export default defineConfig({
     viewportWidth: 1920,
     viewportHeight: 1080,
     numTestsKeptInMemory: 20,
-    video: true,
-    videosFolder: "cypress/videos",
+    // video: true,
+    // videosFolder: "cypress/videos",
     specPattern: [
       "cypress/e2e/user-journeys/admin/features.cy.ts",
       "cypress/e2e/user-journeys/admin/users.cy.ts",
