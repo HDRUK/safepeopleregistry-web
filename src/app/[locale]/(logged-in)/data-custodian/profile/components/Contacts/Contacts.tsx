d39eaea0b8801147d274ec59a997a6dc5a2a6c12
Phil Reeks <phil.reeks@hdruk.ac.uk>
2026-05-05 10:15:21 +0100

    update table

--- src/app/[locale]/(logged-in)/data-custodian/profile/components/Contacts/Contacts.tsx
+++ src/app/[locale]/(logged-in)/data-custodian/profile/components/Contacts/Contacts.tsx
@@ -168,7 +168,7 @@ export default function Contacts() {
             additionalColumns={[
               {
                 accessorKey: "action",
-                header: "Actions",
+                header: "",
                 cell: renderActionMenuCell,
               },
             ]}
