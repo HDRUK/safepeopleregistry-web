ac78d8dabcc3e88d404c16ced97ce06dc1091cac
Sahar.Batol <Sahar.Batol@hdruk.ac.uk>
2026-05-13 15:10:38 +0100

    fix code formatting

--- src/app/[locale]/(logged-in)/organisation/profile/components/SectorSizeAndWebsite/SectorSizeAndWebsite.tsx
+++ src/app/[locale]/(logged-in)/organisation/profile/components/SectorSizeAndWebsite/SectorSizeAndWebsite.tsx
@@ -57,7 +57,9 @@ export default function SectorSizeAndWebsite() {
           .string()
           .required(tForm("websiteRequiredInvalid"))
           .matches(VALIDATION_URL, tForm("websiteFormatInvalid")),
-        organisation_size: yup.number().required(tForm("organisationSizeRequiredInvalid")),
+        organisation_size: yup
+          .number()
+          .required(tForm("organisationSizeRequiredInvalid")),
       }),
     []
   );
