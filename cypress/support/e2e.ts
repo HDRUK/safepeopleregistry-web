4797706bf4969dda603094cc20577f1fbb6770f2
Phil Reeks <phil.reeks@hdruk.ac.uk>
2026-05-13 16:01:21 +0100

    capture 404

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -3,7 +3,8 @@ import "cypress-if";
 import "./commands";
 
 Cypress.on("uncaught:exception", err => {
-  return !(/unknown/i.test(err.message) || /NEXT_REDIRECT/.test(err.message));
+  console.error("App error:", err);
+  return false;
 });
 
 import "./commands";
