bd12e9890ac9dff4c9e08d3a55aebd3ce0430663
Sahar.Batol <Sahar.Batol@hdruk.ac.uk>
2026-05-19 15:23:18 +0100

    add glossary page testing

---
+++ src/app/[locale]/(logged-out)/glossary/components/Glossary/Glossary.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import Glossary from "./Glossary";
+
+// Mock next-intl
+jest.mock("next-intl/server", () => ({
+  getTranslations: async () => {
+    return (key: string) => {
+      const translations: Record<string, string> = {
+        infoTitle: "Glossary",
+      };
+
+      return translations[key];
+    };
+  },
+}));
+
+describe("Glossary Component", () => {
+  it("renders the glossary title", async () => {
+    const Component = await Glossary();
+
+    render(Component);
+
+    expect(screen.getByText("Glossary")).toBeInTheDocument();
+  });
+});
