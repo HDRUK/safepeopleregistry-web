9a3086d2508053a39ff5fb28d311d966033bec67
Sahar.Batol <Sahar.Batol@hdruk.ac.uk>
2026-06-26 10:33:57 +0100

    add micro organisation size option

--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -21,6 +21,9 @@ export default function Results({
   pagination,
   ...restProps
 }: ResultsProps) {
+  console.log(queryState, "queryState");
+  cons;
+
   return (
     <Box
       {...restProps}
