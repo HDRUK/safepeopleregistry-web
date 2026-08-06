bf3cfb6f679f5772cf2e213d83968c6782a6c859
Sahar.Batol <Sahar.Batol@hdruk.ac.uk>
2026-08-06 09:27:55 +0100

    clean up

--- src/consts/router.ts
+++ src/consts/router.ts
@@ -245,10 +245,6 @@ const ROUTES: Routes = {
     path: "/organisation/profile/details/digital-identifiers",
     permissions: [UserGroup.ORGANISATIONS],
   },
-  // profileOrganisationDetailsSectorSizeAndWebsite: {
-  //   path: "/organisation/profile/details/sector-size-and-website",
-  //   permissions: [UserGroup.ORGANISATIONS],
-  // },
   profileOrganisationDetailsSecurityCompliance: {
     path: "/organisation/profile/details/security-compliance",
     permissions: [UserGroup.ORGANISATIONS],
