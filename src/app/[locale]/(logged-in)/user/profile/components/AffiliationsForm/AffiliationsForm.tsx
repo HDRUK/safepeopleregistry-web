4b4575d2ac32dc028e4271b0ecc62781821f3693
Sam Cox <sam.cox@hdruk.ac.uk>
2026-08-11 10:50:08 +0100

    Existing affiliations cannot have their email changed, regardless of their status or whether the email has been verified.

--- src/app/[locale]/(logged-in)/user/profile/components/AffiliationsForm/AffiliationsForm.tsx
+++ src/app/[locale]/(logged-in)/user/profile/components/AffiliationsForm/AffiliationsForm.tsx
@@ -403,6 +403,7 @@ export default function AffiliationsForm({
                         </Box>
                       )
                     }
+                    disabled={!!initialValues}
                   />
                 </Grid>
               )}
