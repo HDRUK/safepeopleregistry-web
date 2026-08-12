8e22cdee8c6fea49bf736d6425b0a041bb86e6dd
Sahar.Batol <Sahar.Batol@hdruk.ac.uk>
2026-08-12 11:05:00 +0100

    remove asterisks when in read only (delegate) view

--- src/app/[locale]/(logged-in)/organisation/profile/components/NameAndAddress/NameAndAddress.tsx
+++ src/app/[locale]/(logged-in)/organisation/profile/components/NameAndAddress/NameAndAddress.tsx
@@ -98,18 +98,30 @@ export default function NameAndAddress() {
         county: yup.string().required(tForm("countyRequiredInvalid")),
         country: yup.string().required(tForm("countryRequiredInvalid")),
         postcode: yup.string().required(tForm("postcodeRequiredInvalid")),
-        first_name: yup.string().required(),
-        last_name: yup.string().required(),
-        department: yup.number().required(),
-        email: yup
-          .string()
-          .email(tForm("emailInvalid"))
-          .required(tForm("emailRequired")),
-        role: yup.string().required(tForm("roleRequiredInvalid")),
-        sro_profile_uri: yup
-          .string()
-          .url(tForm("sroProfileUriInvalid"))
-          .required(tForm("sroProfileUriRequiredInvalid")),
+        first_name: !isDelegate
+          ? yup.string().required()
+          : yup.string().nullable(),
+        last_name: !isDelegate
+          ? yup.string().required()
+          : yup.string().nullable(),
+        department: !isDelegate
+          ? yup.number().required()
+          : yup.number().nullable(),
+        email: !isDelegate
+          ? yup
+              .string()
+              .email(tForm("emailInvalid"))
+              .required(tForm("emailRequired"))
+          : yup.string().nullable(),
+        role: !isDelegate
+          ? yup.string().required(tForm("roleRequiredInvalid"))
+          : yup.string().nullable(),
+        sro_profile_uri: !isDelegate
+          ? yup
+              .string()
+              .url(tForm("sroProfileUriInvalid"))
+              .required(tForm("sroProfileUriRequiredInvalid"))
+          : yup.string().nullable(),
       }),
     [tForm]
   );
