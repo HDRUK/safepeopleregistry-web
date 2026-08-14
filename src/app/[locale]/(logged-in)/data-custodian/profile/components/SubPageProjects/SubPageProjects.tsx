6c9cd0585bed917f4f6caf5c20d4c36b0fe3edf1
Sahar.Batol <Sahar.Batol@hdruk.ac.uk>
2026-08-14 13:45:13 +0100

    add back to projects button

--- src/app/[locale]/(logged-in)/data-custodian/profile/components/SubPageProjects/SubPageProjects.tsx
+++ src/app/[locale]/(logged-in)/data-custodian/profile/components/SubPageProjects/SubPageProjects.tsx
@@ -4,6 +4,9 @@ import { ResearcherProject } from "@/types/application";
 import { useEffect } from "react";
 import { PageTabs, ProjectsSubTabs } from "../../consts/tabs";
 import SubTabsContents from "../SubsTabContents";
+import { Box, Link, Typography } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { useTranslations } from "next-intl";
 
 interface PageProps {
   projectData: ResearcherProject;
@@ -12,8 +15,10 @@ interface PageProps {
     id: number;
   };
 }
+const NAMESPACE_TRANSLATION_CUSTODIAN_PROJECT = "CustodianProfile.Project";
 
 export default function SubPageProjects({ params, projectData }: PageProps) {
+  const t = useTranslations(NAMESPACE_TRANSLATION_CUSTODIAN_PROJECT);
   const { subTabId, id } = params;
   const tabId = PageTabs.PROJECTS;
 
@@ -30,9 +35,19 @@ export default function SubPageProjects({ params, projectData }: PageProps) {
 
   return (
     project?.id === Number(id) && (
-      <PageBodyContainer heading={project.title}>
-        <SubTabsContents tabId={tabId} subTabId={subTabId} id={id} />
-      </PageBodyContainer>
+      <>
+        <Link href={`/data-custodian/profile/projects`}>
+          <Box sx={{ display: "flex", alignItems: "center", mb: 2 }}>
+            <ArrowBackIcon />
+            <Typography sx={{ ml: 1, textDecoration: "underline" }}>
+              {t("backToProjects")}
+            </Typography>
+          </Box>
+        </Link>
+        <PageBodyContainer heading={project.title}>
+          <SubTabsContents tabId={tabId} subTabId={subTabId} id={id} />
+        </PageBodyContainer>
+      </>
     )
   );
 }
