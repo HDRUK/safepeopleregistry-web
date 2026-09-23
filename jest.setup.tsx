4ac9ffb7ff6815abc8b4f5838b1a780e18dbede5
Sam Cox <sam.cox@hdruk.ac.uk>
2026-09-23 15:22:16 +0100

    Add mock for invite_to_contact_superadmin

--- jest.setup.tsx
+++ jest.setup.tsx
@@ -475,6 +475,9 @@ async function mockFetch(url: string, init?: RequestInit) {
         }),
       });
     }
+    case `${process.env.NEXT_PUBLIC_API_V1_URL}/organisations/1/invite_to_contact_superadmin`: {
+      return mock200Json(1);
+    }
     case `${process.env.NEXT_PUBLIC_API_V1_URL}/organisations/1/invite_user`: {
       return mock200Json({
         data: true,
